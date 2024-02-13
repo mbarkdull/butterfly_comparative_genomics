780cc9b83c51f9c2072aeb45b606f21bfeb8092a
noahkbrady <nkb53@cornell.edu>
2024-02-13 16:24:05 -0500

    debugging

--- scripts/02_01_single_transdecoder_run.sh
+++ scripts/02_01_single_transdecoder_run.sh
@@ -8,10 +8,11 @@ do
 	export cdsFileName=$line
 	# Copy the CDS
 	cp ./01_RawCDSFiles/$cdsFileName 02_translated_cds_files
-	cd 02_translated_cds_files
+	cd ./02_translated_cds_files
 	export speciesCode=`echo "$line" | awk -F'_' '{print $1}'`
 	mkdir ./$speciesCode/
 	cd ./$speciesCode/
+	mv ../$cdsFileName ./
 	# Now we can run Transdecoder on the cleaned file:
     echo "First, attempting TransDecoder run on $cdsFileName"
     /programs/TransDecoder-v5.5.0/TransDecoder.LongOrfs -t $cdsFileName
