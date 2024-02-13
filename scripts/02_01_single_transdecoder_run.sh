ae45716b838b20c35bac7d2d94b3ed7a2d1d4f11
noahkbrady <nkb53@cornell.edu>
2024-02-13 16:07:12 -0500

    moving each Transdecoder run into it's own output directory

--- scripts/02_01_single_transdecoder_run.sh
+++ scripts/02_01_single_transdecoder_run.sh
@@ -8,9 +8,15 @@ do
 	export cdsFileName=$line
 	cp ./01_RawCDSFiles/$cdsFileName 02_translated_cds_files
 	cd 02_translated_cds_files
+
+	export speciesCode=`echo "$line" | awk -F'_' '{print $1}'`
+	mkdir ./$speciesCode/
+	cd ./$speciesCode/
+	mv ../$cdsFileName ./
 	# Now we can run Transdecoder on the cleaned file:
     echo "First, attempting TransDecoder run on $cdsFileName"
     /programs/TransDecoder-v5.5.0/TransDecoder.LongOrfs -t $cdsFileName
     /programs/TransDecoder-v5.5.0/TransDecoder.Predict -t $cdsFileName --single_best_only
+    cd ../
     cd ../
 done < $1
