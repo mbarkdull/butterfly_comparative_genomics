d71e98acf6ebd8345c5d619838907bf17eeb079a
mbarkdull <meganbarkdull@yahoo.com>
2025-04-22 12:03:11 -0700

    Switching to run directly on nucleotide sequences.

--- scripts/03_orthofinder.sh
+++ scripts/03_orthofinder.sh
@@ -6,25 +6,25 @@
 mkdir ./03_OrthoFinder/
 mkdir ./03_OrthoFinder/fasta
 
-for file in ./02_AAFiles/*
+for file in ./01_RawCDSFiles/*
 do
   # Extract the species code:
   export speciesCode=`echo "$file" | awk -F'_' '{print $2}' | awk -F'/' '{print $2}'`
   echo This is the species code: $speciesCode
   # Copy the proteins file into the orthofinder directory:
-  cp ./02_AAFiles/$speciesCode"_aa.fasta" ./03_OrthoFinder/fasta/
+  cp ./01_RawCDSFiles/$speciesCode"_cds.fasta" ./03_OrthoFinder/fasta/
   # Append a species code to each gene name:
   export samplePrefix=$speciesCode"_"
-  sed -i .backup "s/>/>$samplePrefix/g" "./03_OrthoFinder/fasta/"$speciesCode"_aa.fasta"
+  sed -i .backup "s/>/>$samplePrefix/g" "./03_OrthoFinder/fasta/"$speciesCode"_cds.fasta"
   rm ./03_OrthoFinder/fasta/*.backup
   # One of the species also has periods (.) for some reason in the gene sequences, so get rid of these:
-  sed -i .backup '/^>/! s/\./-/g' "./03_OrthoFinder/fasta/"$speciesCode"_aa.fasta"
+  sed -i .backup '/^>/! s/\./-/g' "./03_OrthoFinder/fasta/"$speciesCode"_cds.fasta"
   rm ./03_OrthoFinder/fasta/*.backup
 done
 
 # Download the Bombyx mori proteins as an outgroup:
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/030/269/925/GCF_030269925.1_ASM3026992v2/GCF_030269925.1_ASM3026992v2_protein.faa.gz -O bomo.gz
-gunzip -c bomo.gz > ./03_OrthoFinder/fasta/bomo_aa.fasta
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/030/269/925/GCF_030269925.1_ASM3026992v2/GCF_030269925.1_ASM3026992v2_cds_from_genomic.fna.gz -O bomo.gz
+gunzip -c bomo.gz > ./03_OrthoFinder/fasta/bomo_cds.fasta
 
 mkdir -p ./tmp
 #source /programs/anaconda3/bin/activate orthofinder-3.0.1b1
@@ -35,7 +35,7 @@ mkdir -p ./tmp
   # -f: directory with input data
   # -d: run on nucleotide sequences rather than amino acid sequences
   # -M: infer multiple sequence aligments and gene trees.
-orthofinder -S diamond -t $1 -f ./03_OrthoFinder/fasta/ -M msa
+orthofinder -S diamond -t $1 -f ./03_OrthoFinder/fasta/ -d
 
 # Now we need to use an orthofinder utility to extract just the orthogroups we are interested in (the N1 clade)
 cd ./03_OrthoFinder/
